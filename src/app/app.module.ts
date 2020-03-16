068772a3d87e6a31b4dc3ed1f26bb517306280c1
Melanie <melaniemarval@gmail.com>
2020-03-16 17:55:45 -0400

    add drawer in close-to-me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouteReuseStrategy} from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
-import {SplashScreen} from '@ionic-native/splash-screen/ngx';
-import {StatusBar} from '@ionic-native/status-bar/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppRoutingModule } from './app-routing.module';
 
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app-routing.module';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {BusinessDetailsPage} from './pages/details-business/business-details';
-import {SelectIconPage} from './pages/details-business/select-icon/select-icon';
-import {OrdersPage} from './pages/orders/orders';
+// Components
+import { AppComponent } from './app.component';
+import { BusinessDetailsPage } from './pages/details-business/business-details';
+import { SelectIconPage } from './pages/details-business/select-icon/select-icon';
+import { OrdersPage } from './pages/orders/orders';
 
 @NgModule({
     declarations: [AppComponent, OrdersPage, BusinessDetailsPage, SelectIconPage],
--- src/app/pages/tabs/close-to-me/close-to-me.module.ts
+++ src/app/pages/tabs/close-to-me/close-to-me.module.ts
@@ -1,17 +1,21 @@
-import {IonicModule} from '@ionic/angular';
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {FormsModule} from '@angular/forms';
-import {CloseToMeRoutingModule} from './close-to-me-routing.module';
-import {CloseToMePage} from './close-to-me';
-import {NearbyPage} from './nearby/nearby';
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CloseToMeRoutingModule } from './close-to-me-routing.module';
+import { IonBottomDrawerModule } from 'ion-bottom-drawer';
+
+// Components
+import { CloseToMePage } from './close-to-me';
+import { NearbyPage } from './nearby/nearby';
 
 @NgModule({
     imports: [
         IonicModule,
         CommonModule,
         FormsModule,
-        CloseToMeRoutingModule
+        CloseToMeRoutingModule,
+        IonBottomDrawerModule,
     ],
     declarations: [CloseToMePage, NearbyPage]
 })
